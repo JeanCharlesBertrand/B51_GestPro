6cd3030c6f633faccbb0ba3114a55624f3cf0d17
shadarnook <vincentrudel@hotmail.com>
2018-12-04 18:59:28 -0500

    test de push a partir d'un fichier .bat

--- 2018_GestPro_serveur/gp_modules/gp_modelisation/push.bat
+++ 2018_GestPro_serveur/gp_modules/gp_modelisation/push.bat
@@ -4,4 +4,4 @@ call git.exe config --global user.email "vincentrudel@hotmail.com"
 call git.exe config --global user.password "Yolo54321" 
 call git.exe add *
 call git.exe commit -m "test de push a partir d'un fichier .bat"
-call git.exe push origin master --repo https://shadarnook:password@domain.name/name/repo.git
+call git.exe push origin master --repo https://shadarnook:Yolo54321@domain.name/name/repo.git
