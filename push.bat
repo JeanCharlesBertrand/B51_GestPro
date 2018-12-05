62b6274b1113cd12eecc6de105cd693fc0ebfea7
shadarnook <vincentrudel@hotmail.com>
2018-12-04 19:11:18 -0500

    nouveau test

--- push.bat
+++ push.bat
@@ -4,4 +4,4 @@ call git.exe config --global user.email "vincentrudel@hotmail.com"
 call git.exe config --global user.password "Yolo54321"
 call git.exe config remote.origin.url https://shadarnook:Yolo54321@github.com/JeanCharlesBertrand/B51_GestPro
 call git.exe add *
-call git.exe commit -m "test de push a partir d'un fichier .bat"
+call git.exe commit -m "nouveau test"
