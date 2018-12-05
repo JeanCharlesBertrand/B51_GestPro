2d8f7ad24ff96a62703ff8535532eeeb08a23c5a
shadarnook <vincentrudel@hotmail.com>
2018-12-05 09:34:07 -0500

    Push Vincent2

--- 2018_GestPro_serveur/gp_modules/gp_modelisation/gp_modelisation_vue.py
+++ 2018_GestPro_serveur/gp_modules/gp_modelisation/gp_modelisation_vue.py
@@ -147,8 +147,8 @@ class Vue():
             bg="#4C9689",                                             
             relief = "raised",
             font = ("Courier New", 14, "bold"),
-            fg = "#dbdbdb")
-			#command=self.loginclient) 
+            fg = "#dbdbdb",
+			command=self.insertLineToTable) 
 			
 		btnSave=Button(                                    
             text=" Save ",
@@ -166,6 +166,14 @@ class Vue():
             fg = "#dbdbdb")
 			#command=self.loginclient)  
 
+			
+		self.labelNomCetteTable = Label(
+			self.cadresplash,
+			bd=1,
+			text="Nom de la table: ",fg="#4C9689",
+			font = ("Courier New", 10, "bold"),
+			bg="#282E3F")
+		self.labelDefaultChamp.place(x=500, y=140)
 		self.entryDefaultChamp.place(x=500, y= 400)
 		self.labelDefaultChamp.place(x=500, y=140)
 		self.entryNNChamp.place(x=420, y= 400)
@@ -177,7 +185,9 @@ class Vue():
 		self.entryTypeChamp.place(x=260, y= 400)
 		self.labelTypeChamp.place(x=270, y=140)
 
-			
+		self.lineEntries = [ self.entryNomChamp, self.entryTypeChamp, self.entryKeyChamp, self.entryNNChamp, self.entryDefaultChamp]
+		
+
 		self.canevasplash.create_window(580,80,window=btnNew,width=60,height=30)
 		self.canevasplash.create_window(602,412,window=btnAdd,width=30,height=30)
 		self.canevasplash.create_window(580,120,window=btnSave,width=60,height=30)
@@ -186,20 +196,31 @@ class Vue():
 		self.entryNomTable.place(x=320, y= 70)
 		self.labelNomTable.place(x=160, y=70)
 		self.labelListeTables.place(x= 20, y=70)
-		listTables = Listbox(self.cadresplash,height=20, bg="#002887")
-		listTables.place(x= 20, y=100)
-		listNom = Listbox(self.cadresplash,  height=500, bg="#002887")
-		listNom.place(x= 180, y=160,width=65, height=235)
-		listType = Listbox(self.cadresplash, width=20, bg="#002887")
-		listType.place(x= 260, y=160,width=65, height=235)
-		listKey = Listbox(self.cadresplash, width=20, bg="#002887")
-		listKey.place(x= 340, y=160,width=65, height=235)
-		listNN = Listbox(self.cadresplash, width=20, bg="#002887")
-		listNN.place(x= 420, y=160,width=65, height=235)
-		listDefault = Listbox(self.cadresplash, width=20, bg="#002887")
-		listDefault.place(x= 500, y=160,width=65, height=235)
+		self.listTables = Listbox(self.cadresplash,height=20, bg="#002887")
+		self.listTables.place(x= 20, y=100)
+		self.listNom = Listbox(self.cadresplash,  height=500, bg="#002887", font = ('Courier New',14), fg = 'white')
+		self.listNom.place(x= 180, y=160,width=65, height=235)
+		self.listType = Listbox(self.cadresplash, width=20, bg="#002887", font = ('Courier New',14), fg = 'white')
+		self.listType.place(x= 260, y=160,width=65, height=235)
+		self.listKey = Listbox(self.cadresplash, width=20, bg="#002887", font = ('Courier New',14), fg = 'white')
+		self.listKey.place(x= 340, y=160,width=65, height=235)
+		self.listNN = Listbox(self.cadresplash, width=20, bg="#002887", font = ('Courier New',14), fg = 'white')
+		self.listNN.place(x= 420, y=160,width=65, height=235)
+		self.listDefault = Listbox(self.cadresplash, width=20, bg="#002887", font = ('Courier New',14), fg = 'white')
+		self.listDefault.place(x= 500, y=160,width=65, height=235)
 		
+		self.listBoxes = [ self.listNom, self.listType, self.listKey, self.listNN, self.listDefault  ]
 		
+	def insertLineToTable(self):
+		line = [entry.get() for entry in self.lineEntries]
+		for i in range(5):
+			listbox = self.listBoxes[i]
+			listbox.insert(listbox.size(), line[i])
+		
+		#print(line)
+	
+	
+	
 		
 	def fermerfenetre(self):
 		print("ONFERME la fenetre")
