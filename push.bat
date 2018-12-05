7779f6f925a0dcfa3c01a5c8b8fc8c8f3233baa3
shadarnook <vincentrudel@hotmail.com>
2018-12-04 19:10:35 -0500

    test de push a partir d'un fichier .bat

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 cd /d %~dp0
 call git.exe config --global user.name "shadarnook"
 call git.exe config --global user.email "vincentrudel@hotmail.com"
-call git.exe config --global user.password "Yolo54321" 
+call git.exe config --global user.password "Yolo54321"
+call git.exe config remote.origin.url https://shadarnook:Yolo54321@github.com/JeanCharlesBertrand/B51_GestPro
 call git.exe add *
 call git.exe commit -m "test de push a partir d'un fichier .bat"
-call git.exe push origin master --repo https://shadarnook:Yolo54321@domain.name/JeanCharlesBertrand/B51_GestPro
