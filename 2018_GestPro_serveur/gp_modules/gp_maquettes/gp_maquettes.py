829310510a394c3ffb71714602ab4960d1ec798c
Desgagné Julien <1513318@decinfo.cvm>
2018-12-17 09:07:55 -0500

    maquette

--- 2018_GestPro_serveur/gp_modules/gp_maquettes/gp_maquettes.py
+++ 2018_GestPro_serveur/gp_modules/gp_maquettes/gp_maquettes.py
@@ -12,23 +12,51 @@ from helper import Helper as hlp
 from IdMaker import Id
 from xmlrpc.client import ServerProxy
 
+class Mod_maquette():
+    def __init__(self, nomMaquette):
+        self.id = None
+        self.nom = nomMaquette
+        self.formes = []
+        
+    def ajouteForme(self, forme, couleur, largeur, coordonnee):
+        forme = Forme(forme, couleur, largeur, coordonnee)
+        self.formes.append(forme)
+        
+        
+class Forme():
+    def __init__(self, forme, couleur, largeur, coordonnee):
+        self.typeForme = forme
+        self.coordonnees = coordonnee
+        self.couleur = couleur
+        self.largeur = largeur
+        
+
 class Controleur():
     def __init__(self):
         print("IN CONTROLEUR",sys.argv)
         self.createurId=Id
         self.modele=None
-        self.vue=Vue(self)
-        self.vue.root.mainloop()
         self.idProjet=int(sys.argv[4])
         self.ipserveur=sys.argv[2]
         self.nodeport=sys.argv[3]
         self.serveur=None
         self.lierServeur()
+        self.vue=Vue(self)
+        self.vue.root.mainloop()
+        
+    def ajouteForme(self, forme, couleur, largeur, coordonnee):
+        print("In controleur:", forme, couleur, largeur, coordonnee)
+        self.modele.ajouteForme(forme, couleur, largeur, coordonnee)
 
 
     def lierServeur(self):
         ad="http://"+self.ipserveur+":"+self.nodeport
         self.serveur=ServerProxy(ad)
         
+    def creerMaquette(self, nomMaquette):
+        self.modele = Mod_maquette(nomMaquette)
+
+
+
 if __name__ == '__main__':
     c=Controleur()
---
+++ 2018_GestPro_serveur/gp_modules/gp_maquettes/test.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+
+class App(tk.Frame):
+    def __init__( self, parent):
+        tk.Frame.__init__(self, parent)
+        self._createVariables(parent)
+        self._createCanvas()
+        self._createCanvasBinding()
+
+    def _createVariables(self, parent):
+        self.parent = parent
+        self.rectx0 = 0
+        self.recty0 = 0
+        self.rectx1 = 0
+        self.recty1 = 0
+        self.rectid = None
+        self.move = False
+
+    def _createCanvas(self):
+        self.canvas = tk.Canvas(self.parent, width = 800, height = 400,
+                                bg = "white" )
+        self.canvas.grid(row=0, column=0, sticky='nsew')
+
+    def _createCanvasBinding(self):
+        self.canvas.bind( "<Button-1>", self.startRect )
+        self.canvas.bind( "<ButtonRelease-1>", self.stopRect )
+        self.canvas.bind( "<Motion>", self.movingRect )
+
+    def startRect(self, event):
+        self.move = True
+        #Translate mouse screen x0,y0 coordinates to canvas coordinates
+        self.rectx0 = self.canvas.canvasx(event.x)
+        self.recty0 = self.canvas.canvasy(event.y) 
+        #Create rectangle
+        self.rect = self.canvas.create_rectangle(
+            self.rectx0, self.recty0, self.rectx0, self.recty0, fill="#4eccde")
+        #Get rectangle's canvas object ID
+        self.rectid = self.canvas.find_closest(self.rectx0, self.recty0, halo=2)
+        print('Rectangle {0} started at {1} {2} {3} {4} '.
+              format(self.rect, self.rectx0, self.recty0, self.rectx0,
+                     self.recty0))
+
+    def movingRect(self, event):
+        if self.move: 
+            #Translate mouse screen x1,y1 coordinates to canvas coordinates
+            self.rectx1 = self.canvas.canvasx(event.x)
+            self.recty1 = self.canvas.canvasy(event.y)
+            #Modify rectangle x1, y1 coordinates
+            self.canvas.coords(self.rectid, self.rectx0, self.recty0,
+                          self.rectx1, self.recty1)
+            print('Rectangle x1, y1 = ', self.rectx1, self.recty1)
+
+    def stopRect(self, event):
+        self.move = False
+        #Translate mouse screen x1,y1 coordinates to canvas coordinates
+        self.rectx1 = self.canvas.canvasx(event.x)
+        self.recty1 = self.canvas.canvasy(event.y) 
+        #Modify rectangle x1, y1 coordinates (final)
+        self.canvas.coords(self.rectid, self.rectx0, self.recty0,
+                      self.rectx1, self.recty1)
+        print('Rectangle ended')
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry( "600x400" )
+    app = App(root)
+    root.mainloop()
